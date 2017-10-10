41c990336aee5674c865dab0c61820bda5158791
Darren Spruell <darren.spruell@riskiq.net>
2017-10-10 00:16:52 -0700

    Remove quotes from rule classtype option

--- ampt_generator/settings.py
+++ ampt_generator/settings.py
@@ -27,7 +27,7 @@ DB_INIT_VAL = 0
 # Packet generator / NIDS rule defaults
 RULE_STRUCTURE = ('alert ip any any -> any any (msg:"NIDS HEALTH MONITORING"; '
                   'content:"|%s|"; fast_pattern:only; '
-                  'classtype:"not-suspicious"; '
+                  'classtype:not-suspicious; '
                   'reference:url,github.com/nids-io/ampt-generator; '
                   'sid:3900001; rev:1;)')
 PACKET_CONTENT = '0c56f99b-cf66-4679-9be2-3b6384c27586.NIDS_HEALTH_CHECK'
